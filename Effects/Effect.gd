096183ea33047886aa41e615ca291fb87ad0a311
copperpocket <m.mulek45@gmail.com>
2022-03-13 14:04:27 -0500

    Update Effect.gd
    Small debug warning remover

--- Effects/Effect.gd
+++ Effects/Effect.gd
@@ -2,6 +2,7 @@ extends AnimatedSprite
 
 func _ready() -> void:
 	frame = 0
+	# warning-ignore:return_value_discarded
 	connect("animation_finished", self, "_on_animation_finished")
 	play("Animate")
 
