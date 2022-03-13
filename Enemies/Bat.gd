5a81a6f0703f5bd7b332f4642c0db13767443784
copperpocket <m.mulek45@gmail.com>
2022-03-12 18:03:55 -0600

    Initial import
    Importing existing project, on video 16 in tutorial series by HeartBeast on YouTube.
    
    Link to start of playlist:
    https://youtu.be/mAbG8Oi-SvQ

---
+++ Enemies/Bat.gd
@@ -0,0 +1,22 @@
+extends KinematicBody2D
+
+const EnemyDeathEffect = preload("res://Effects/EnemyDeathEffect.tscn")
+const FRICTION = 200
+
+var knockback = Vector2.ZERO
+
+onready var stats = $Stats
+
+func _physics_process(delta: float) -> void:
+	knockback = knockback.move_toward(Vector2.ZERO, FRICTION * delta)
+	knockback = move_and_slide(knockback)
+
+func _on_Hurtbox_area_entered(area: Area2D) -> void:
+	stats.health -= area.damage
+	knockback = area.knockback_vector * 120
+
+func _on_Stats_no_health() -> void:
+	queue_free()
+	var enemyDeathEffect = EnemyDeathEffect.instance()
+	get_parent().add_child(enemyDeathEffect)
+	enemyDeathEffect.global_position = global_position
