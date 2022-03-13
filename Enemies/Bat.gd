b273ec81dc58675a00c46fe57e3a79a84ec07017
copperpocket <m.mulek45@gmail.com>
2022-03-13 12:17:21 -0500

    P21 - Bat Wander State
    -Added to the wander state that has been passing up until now, enemy will periodically wander a certain distance from it's origin and attempt to return to that origin upon chasing the player and after exiting it's detection range

--- Enemies/Bat.gd
+++ Enemies/Bat.gd
@@ -5,6 +5,7 @@ const EnemyDeathEffect = preload("res://Effects/EnemyDeathEffect.tscn")
 export var ACCELERATION = 300
 export var MAX_SPEED = 50
 export var FRICTION = 200
+export var WANDER_TARGET_RANGE = 4
 
 enum {
 	IDLE,
@@ -22,29 +23,56 @@ onready var playerDetectionZone = $PlayerDetectionZone
 onready var sprite = $AnimatedSprite
 onready var hurtBox = $Hurtbox
 onready var softCollision = $SoftCollision
+onready var wanderController = $WanderController
+
+func _ready() -> void:
+	state = get_random_state([IDLE, WANDER])
 
 func _physics_process(delta: float) -> void:
 	knockback = knockback.move_toward(Vector2.ZERO, FRICTION * delta)
 	knockback = move_and_slide(knockback)
-	
+
 	match state:
 		IDLE:
 			velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 			seek_player()
+			if wanderController.get_time_left() == 0:
+				update_wander()
 		WANDER:
-			pass
+			seek_player()
+			if wanderController.get_time_left() == 0:
+				update_wander()
+			accelerate_towards_point(wanderController.target_position, delta)
+			if global_position.distance_to(wanderController.target_position) <= WANDER_TARGET_RANGE:
+				update_wander()
 		CHASE:
 			var player = playerDetectionZone.player
 			if player != null:
-				#var direction = (player.global_position - global_position).normalized()
-				var direction = position.direction_to(player.global_position)
-				velocity = velocity.move_toward(direction * MAX_SPEED, ACCELERATION * delta)
+				accelerate_towards_point(player.global_position, delta)
 			else: state = IDLE
 			sprite.flip_h = velocity.x < 0
-	
+
 	if softCollision.is_colliding():
 		velocity += softCollision.get_push_vector() * delta * 400
 	velocity = move_and_slide(velocity)
+	
+func accelerate_towards_point(point, delta):
+	#var direction = (player.global_position - global_position).normalized()
+	var direction = position.direction_to(point)
+	velocity = velocity.move_toward(direction * MAX_SPEED, ACCELERATION * delta)
+	sprite.flip_h = velocity.x < 0
+
+func update_wander():
+	state = get_random_state([IDLE, WANDER])
+	wanderController.start_wander_timer(rand_range(1, 3))
+
+func seek_player():
+	if playerDetectionZone.can_see_player():
+		state = CHASE
+
+func get_random_state(state_list):
+	state_list.shuffle()
+	return state_list.pop_front()
 
 func _on_Hurtbox_area_entered(area: Area2D) -> void:
 	stats.health -= area.damage
@@ -57,6 +85,3 @@ func _on_Stats_no_health() -> void:
 	get_parent().add_child(enemyDeathEffect)
 	enemyDeathEffect.global_position = global_position
 
-func seek_player():
-	if playerDetectionZone.can_see_player():
-		state = CHASE
